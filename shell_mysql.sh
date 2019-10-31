cfeaa50ce44a6dcf1b6e71c5bbd5c50d5f9e5ebc
yuuri <houjie95@foxmail.com>
2019-10-31 14:34:14 +0800

    add sum count

--- shell_mysql.sh
+++ shell_mysql.sh
@@ -5,6 +5,7 @@ echo -e '\033[36m=======================\033[0m'
 var=`ls -l /home/pytorch |grep ^d  |awk '{print $9}'`
 count=`ls -l /home/pytorch |grep ^d  |awk '{print $9}'|wc -l`
 echo  -e "====Total Dir Count is $count ===="
+sum=0
 for i in $var;
 do
 echo -e  "\033[36mProcess in $i\033[0m"
@@ -12,6 +13,8 @@ let num+=1;
 #echo "And MySQL Result is following"
 # 使用awk NR==n 只获取第n行的内容
 sql=`mysql -uroot -proot -D git -e "select count(1) as $i from request_file where project='pytorch/pytorch' and file_name like '$i%'" |awk NR==2`
+# 求和
+let sum+=$sql
 echo "$sql"
 # 使用$'\n'在变量中添加换行符
 #result=$i:$sql$'\n'
@@ -29,5 +32,6 @@ echo "Result is following"
 var=${array[*]}
 # 使用$(var// /$'\n')将默认以空格为分隔符的数组输出转为以换行符为分隔符
 echo -e   "\033[36m${var// /$'\n'}\033[0m"
+echo -e  "Total Dir File is $sum "
 }
 get_mysql_result
