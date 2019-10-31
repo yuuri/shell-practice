2048771cad09a874f59b3c2ca7653e80350b1bb5
yuuri <houjie95@foxmail.com>
2019-10-31 12:26:32 +0800

    add annotation

--- shell_mysql.sh
+++ shell_mysql.sh
@@ -9,9 +9,13 @@ for i in $var;
 do
 #echo "Now Dir is $i"
 #echo "And MySQL Result is following"
+# 使用awk NR==n 只获取第n行的内容
 sql=`mysql -uroot -proot -D git -e "select count(1) as $i from request_file where project='pytorch/pytorch' and file_name like '$i%'" |awk NR==2`
+# 使用$'\n'在变量中添加换行符
 result=$i:$sql$'\n'
+
 array[$num]=$result
+#使用let 方法定义一个自增变量
 let num+=1;
 #echo -e '\033[36m====== result is ======\033[0m'
 #echo -e "$sql"
@@ -19,4 +23,5 @@ let num+=1;
 #echo -e '\033[36m======== Done =========\033[0m'
 #echo " "
 done
+#使用${array[@]} 或者 ${array[*]}获得一个变量的所有值
 echo -e  "\033[36m${array[@]}\033[0m"
