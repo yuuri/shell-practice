c77a170146b7ef0557b0b322a10c677f3a996e72
yuuri <i@yuuriso.com>
2019-11-20 21:16:37 +0800

    add new sql

--- backup.sql
+++ backup.sql
@@ -3,4 +3,5 @@ select count(1) from (select file_name,count(file_name) as file_count from reque
 select file_name,count(file_name) as file_count from request_file where project='pytorch/pytorch' group by file_name  having file_count>10;
 select count(1) from (select file_name,count(file_name) as file_count from request_file where project='pytorch/pytorch' group by file_name  having file_count>10) t;
 select @num:=@num+1,result.* from (select file_name,count(file_name) as file_count from request_file where project='pytorch/pytorch' group by file_name having file_count>10) as result,(select @num:=0) r;
+select @num:=@num+1 as count,result.* from (select distinct pull_number,comment_user from comments where project='ceph/ceph' order by pull_number,comment_date) result,(select @num:=0) r limit 10
 
